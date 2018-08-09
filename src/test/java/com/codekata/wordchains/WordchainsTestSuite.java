8ec88c2c4a47c11c05ba9f45a8ac7c2896a3198c
blue0403 <artur.lichaj@gmail.com>
2018-08-09 14:41:55 +0200

    Create a scheme of tests

---
+++ src/test/java/com/codekata/wordchains/WordchainsTestSuite.java
@@ -0,0 +1,62 @@
+package com.codekata.wordchains;
+
+import org.junit.Test;
+
+public class WordchainsTestSuite {
+
+    @Test
+    public void testLoadDictionaryAsFile() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void testLoadDictionaryFromLink() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void testGetChain() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void testGetChainWithWordOutsideDictionary() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void testGetChainWithDictionaryAsFile() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void testGetChainWithDictionaryFromLink() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void testTimeOfGetChainWithDictionaryAsFile() {
+        //Given
+        //When
+        //Then
+    }
+
+    @Test
+    public void testTimeOfGetChainWithDictionaryFromLink() {
+        //Given
+        //When
+        //Then
+    }
+}
