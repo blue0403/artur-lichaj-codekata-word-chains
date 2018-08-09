0b5330e3ac632efb153ee405ded7a5a23705389d
blue0403 <artur.lichaj@gmail.com>
2018-08-09 16:41:59 +0200

    Add implementation of method loadDictionary() - load from file

--- src/main/java/com/codekata/wordchains/DictionaryFromFileLoader.java
+++ src/main/java/com/codekata/wordchains/DictionaryFromFileLoader.java
@@ -1,10 +1,30 @@
 package com.codekata.wordchains;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class DictionaryFromFileLoader implements DictionaryLoader {
 
     @Override
     public String[] loadDictionary() {
-        //Load dictionary from file
-        return new String[0];
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/wordlist.txt").getFile());
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(file)))) {
+            List<String> words = new ArrayList<>();
+            String word;
+            while (scanner.hasNextLine()) {
+                word = scanner.nextLine();
+                words.add(word);
+            }
+            scanner.close();
+            return words.toArray(new String[words.size()]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
