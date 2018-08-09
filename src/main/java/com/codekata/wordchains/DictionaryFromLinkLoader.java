aa964e1a04ef487f7e53df0d363c0c30da6281b4
blue0403 <artur.lichaj@gmail.com>
2018-08-09 16:47:19 +0200

    Add implementation of method loadDictionary() - load from link

--- src/main/java/com/codekata/wordchains/DictionaryFromLinkLoader.java
+++ src/main/java/com/codekata/wordchains/DictionaryFromLinkLoader.java
@@ -1,10 +1,29 @@
 package com.codekata.wordchains;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DictionaryFromLinkLoader implements DictionaryLoader {
 
     @Override
     public String[] loadDictionary() {
-        //Load dictionary from link
-        return new String[0];
+        try (InputStream inputStream = new URL("http://codekata.com/data/wordlist.txt").openConnection().getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            List<String> dict = new ArrayList<>();
+            String word;
+            while ((word = reader.readLine()) != null) {
+                dict.add(word);
+            }
+            reader.close();
+            return dict.toArray(new String[dict.size()]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
